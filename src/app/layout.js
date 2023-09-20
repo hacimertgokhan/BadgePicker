a7c4d3d5e397382a2028a8dd447cacdf1e8b41d9
Mert Gökhan <hacimertgokhan@gmail.com>
2023-09-20 10:22:04 +0300

    badgepicker-0006

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,10 +18,10 @@ export default function RootLayout({ children }) {
             </span>
             <ul>
                 <li>
-                    <a href="https://instagram.com" id='instagram'><i className="fa-brands fa-instagram"></i></a>
+                    <a href="https://instagram.com/hacimertgokhan" id='instagram'><i className="fa-brands fa-instagram"></i></a>
                 </li>
                 <li>
-                    <a href="https://github.com/mindlymedia" id='discord'><i className="fa-brands fa-github"></i></a>
+                    <a href="https://github.com/hacimertgokhan" id='discord'><i className="fa-brands fa-github"></i></a>
                 </li>
             </ul>
           </div>
@@ -31,10 +31,10 @@ export default function RootLayout({ children }) {
         <br></br>
         <footer>
           <ul>
-            <li><a><i className="fa-solid fa-code-fork"></i> Fork BadgePicker</a></li>
-            <li><a><i className="fa-solid fa-star"></i> Give a star</a></li>
-            <li><a><i className="fa-solid fa-code"></i> Developer</a></li>
-            <li><a><i className="fa-solid fa-box-tissue"></i> Create Issues</a></li>
+            <li><a href='https://github.com/hacimertgokhan/BadgePicker/forks'><i className="fa-solid fa-code-fork"></i> Fork BadgePicker</a></li>
+            <li><a href='https://github.com/hacimertgokhan/BadgePicker'><i className="fa-solid fa-star"></i> Give a star</a></li>
+            <li><a href='https://github.com/hacimertgokhan/'><i className="fa-solid fa-code"></i> Developer</a></li>
+            <li><a href='https://github.com/hacimertgokhan/BadgePicker/issues'><i className="fa-solid fa-box-tissue"></i> Create Issues</a></li>
           </ul>
         </footer>
         <script src="https://kit.fontawesome.com/55460c171b.js" crossOrigin="anonymous"></script>
